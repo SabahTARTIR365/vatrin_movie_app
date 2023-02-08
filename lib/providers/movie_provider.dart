e0784fe74c09b948bd62c033c0661feaf06d5fae
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-02-09 00:07:39 +0200

    add load movies functions for popular and now playing movies in the provider

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/cupertino.dart';
+import 'package:tmdb_api/tmdb_api.dart';
+import 'package:vatrin_movie_app/data/constant.dart';
 
 class MovieProvider extends ChangeNotifier
 {
+  TMDB ?tmdbWithCustomLogs;
+  MovieProvider()
+  {
+    //initialization files
+    initializeApiDatabase();
+    getNowShowingMovies();
+    getPopularMovies();
+  }
 
+  initializeApiDatabase()
+  {
+    tmdbWithCustomLogs = TMDB(
+      ApiKeys(apiKey, readAccessToken),
+      logConfig: const ConfigLogger(
+        showLogs: true,
+        showErrorLogs: true,
+      ),
+    );
+  }
+
+  getNowShowingMovies() async
+  {
+    Map nowShowingResult= await tmdbWithCustomLogs!.v3.movies.getNowPlaying();
+    nowShowingMovies=nowShowingResult['results'];
+    notifyListeners();
+  }
+  getPopularMovies() async {
+    Map popularMoviesResult = await tmdbWithCustomLogs!.v3.movies.getPopular();
+    popularMovies=popularMoviesResult['results'];
+    notifyListeners();
+  }
 
 
 }
