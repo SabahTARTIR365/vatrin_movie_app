3bf181c1e917fcc4b1ce80fe727bb5f47d8bf35a
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-02-09 00:06:29 +0200

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:vatrin_movie_app/providers/movie_provider.dart';
 import 'package:vatrin_movie_app/views/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+
+
+
+class InitApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ChangeNotifierProvider<MovieProvider >(
+        create: (context) {
+          return MovieProvider();
+        },
+        child: const MyApp());
+  }
+}
+
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  HomeScreen(),
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+          primarySwatch: Colors.blue,
+        ),
+        home:HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-late String title;
 
 
 
-  @override
-  Widget build(BuildContext context) {
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("test app"),
-      ),
-      body: Center(
-        child: Column(),
-      ),
-  // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+
+
+
