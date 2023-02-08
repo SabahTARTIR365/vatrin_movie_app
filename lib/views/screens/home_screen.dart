308cd14224980cfdbf6f98f2463e221b57ace834
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-02-08 21:30:47 +0200

    create home screen

---
+++ lib/views/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:vatrin_movie_app/views/widgets/movie_navigation_bar.dart';
+
+import '../../data/constant.dart';
+
+class HomeScreen extends StatelessWidget
+{
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+
+      body:
+        Container(
+          margin: EdgeInsets.fromLTRB(15, 40, 10, 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const MovieNavigationBar(text: 'Vatrin Film', leadingIcon: 'assets/icons/Menu.svg', endIcon: 'assets/icons/Notif.svg',),
+              SizedBox(height: 30),
+              Row(mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Now Showing', style: titleStyle,),
+                ],
+              ),
+              Container(
+
+              )
+            ],
+
+
+          ),
+        ),
+
+    );
+  }
+
+
+}
