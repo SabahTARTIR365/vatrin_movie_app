cb5b913962607d4d3728c705d42bdc1217770e61
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-02-09 00:08:30 +0200

    add popular and now playing movies' lists

--- lib/data/constant.dart
+++ lib/data/constant.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/cupertino.dart';
 
 TextStyle titleStyle =TextStyle(fontFamily: 'Merriweather',fontWeight: FontWeight.w900,fontSize: 16);
+
+final String apiKey = '44003be4f7cea769f0a024d29a57dcc9';
+final String readAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0'
+    'NDAwM2JlNGY3Y2VhNzY5ZjBhMDI0ZDI5YTU3ZGNjOSIsInN1YiI6IjYzZTM2'
+    'ZjNhZmFjNTAyMDA4NDlmMWFlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2Z'
+    'XJzaW9uIjoxfQ.TpZy_6SBnO_IR_lQZ4JYKzvwvjmqbqb0iLha36ljD4I';
+
+
+List popularMovies = [];
+List nowShowingMovies=[];
+
