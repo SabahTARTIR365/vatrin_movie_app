ebf20610a8f5ed629ebb08803eeeb43cc8ad33a5
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-02-09 22:57:39 +0200

    add showing movie list

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -1,10 +1,18 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 import 'package:vatrin_movie_app/data/constant.dart';
 
+import '../models/movie.dart';
+
 class MovieProvider extends ChangeNotifier
 {
   TMDB ?tmdbWithCustomLogs;
+  List ?popularMovies ;
+  List<Movie>?nowShowingMovies;
+
   MovieProvider()
   {
     //initialization files
@@ -27,7 +35,13 @@ class MovieProvider extends ChangeNotifier
   getNowShowingMovies() async
   {
     Map nowShowingResult= await tmdbWithCustomLogs!.v3.movies.getNowPlaying();
-    nowShowingMovies=nowShowingResult['results'];
+    //nowShowingMovies=nowShowingResult['results'];
+  List movies=nowShowingResult['results'];
+   //nowShowingResult['results'].forEach((e) => nowShowingMovies!.add(Movie.fromMap(e)));
+    nowShowingMovies= movies.map( (entry) => Movie.fromMap(entry)).toList();
+
+
+
     notifyListeners();
   }
   getPopularMovies() async {
