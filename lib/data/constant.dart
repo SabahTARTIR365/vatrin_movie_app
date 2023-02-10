4916bb06866838109b66b4f3e73c557891fd17c8
SabahTARTIR365 <sosotarteer2017@gmail.com>
2023-02-10 14:00:59 +0200

    add text style for time widget

--- lib/data/constant.dart
+++ lib/data/constant.dart
@@ -3,7 +3,8 @@ import 'package:flutter/cupertino.dart';
 TextStyle titleStyle =TextStyle(fontFamily: 'Merriweather',fontWeight: FontWeight.w900,fontSize: 16);
 TextStyle subTitleStyle=TextStyle(fontFamily: 'Mulish',fontWeight: FontWeight.w700,fontSize: 14);
 TextStyle subSubTitleStyle=TextStyle(fontFamily: 'Mulish',fontWeight: FontWeight.w400,fontSize: 12, color: appGray);
-
+TextStyle genreTitleStyle=TextStyle(fontFamily: 'Mulish',fontSize: 8, fontWeight: FontWeight.bold, color: Color(0xff88A4E8));
+TextStyle timeTitleStyle=TextStyle(fontFamily: 'Mulish',fontWeight: FontWeight.w400,fontSize: 12);
 final String apiKey = '44003be4f7cea769f0a024d29a57dcc9';
 final String readAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0'
     'NDAwM2JlNGY3Y2VhNzY5ZjBhMDI0ZDI5YTU3ZGNjOSIsInN1YiI6IjYzZTM2'
@@ -14,3 +15,25 @@ final String readAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0'
 List popularMovies = [];
 List nowShowingMovies=[];
 Color appGray= Color(0xff9C9C9C);
+
+Map<String, int> genres={
+  'Action': 28,
+  'Adventure':       12,
+  'Animation'   :    16,
+  'Comedy'       :   35,
+  'Crime'         :  80,
+  'Documentary'    : 99,
+  'Drama'           :18,
+  'Family'        :  10751,
+  'Fantasy'        : 14,
+  'History'         :36,
+  'Horror'          :27,
+  'Music'           :10402,
+  'Mystery'         :9648,
+  'Romance'        :10749,
+  'Science Fiction' :878,
+  'TV Movie'        :10770,
+  'Thriller'       :53,
+  'War'             :10752,
+  'Western'         :37,
+};
